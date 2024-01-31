1d1561cc154913ecaf9890c50fd3f92d9a5891c9
stsoy01 <tsoy@perco.ru>
2024-01-31 18:14:09 +0300

    upd

--- src/app/components/block-status/block-status.component.ts
+++ src/app/components/block-status/block-status.component.ts
@@ -1,4 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
+
+type statusTypes = 'error' | '404' | 'warning'
 
 @Component({
   selector: 'ts-block-status',
@@ -7,4 +9,8 @@ import {Component} from "@angular/core";
 })
 
 export class BlockStatusComponent {
+
+  @Input()
+  public status!: statusTypes;
+
 }
