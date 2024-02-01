6e4d96a07b72ed57c09ac54f5f035a001a16fb0a
stsoy01 <stsoy01@gmail.com>
2024-02-02 00:51:17 +0300

    upd

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, Renderer2, ViewChild } from "@angular/core";
 
 @Component({
   selector: 'ts-select',
@@ -12,17 +12,13 @@ export class SelectComponent {
   public title: string = '';
 
   @Input()
-  public dropdownList: { label: string | number, value: string | number }[] = [
-    { label: 'Item 1', value: 1 },
-    { label: 'Item 2', value: 2 },
-    { label: 'Item 3', value: 3 },
-  ];
+  public dropdownList: { label: string | number, value: string | number }[] = [];
 
   public isDropdownShowed: boolean = false;
 
   public selectedValue: string = 'Value'
 
-  constructor() { }
+
 
   public showDropdownList(item?: any): void {
     if (item?.label) {
