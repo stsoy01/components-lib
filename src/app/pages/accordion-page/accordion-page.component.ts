9ee0f22f520e1cecaf1adb157e418aceace4bb09
stsoy01 <stsoy01@gmail.com>
2024-03-05 23:48:34 +0300

    upd

--- src/app/pages/accordion-page/accordion-page.component.ts
+++ src/app/pages/accordion-page/accordion-page.component.ts
@@ -10,7 +10,9 @@ import { AccordionData } from "src/app/components/accordion/accordion/accordion.
 
 export class AccordionpageComponent {
 
+
   public selectedSwitchSection: number | string = 'Preview'
+  
   public content: AccordionData[] = [
     {
       title: 'Ellie Williams',
@@ -29,10 +31,10 @@ export class AccordionpageComponent {
         'Despite his long, hard-working hours, he still managed to spend quality time with Sarah, as seen in photographs displayed throughout their home showing the two on a cruise, at a carnival with Tommy, and one of Sarah\'s soccer matches. Additionally, the two often went on several hikes together. Sarah also made him take her to every museum in Texas.\n' +
         '\n' +
         'On his 32nd birthday—just hours leading up to the outbreak—Sarah gave him a new watch as a gift to replace the one he had broken months before. This became a cherished memento he manages to keep in the turbulent years ahead.'
-    },    {
+    }, {
       title: 'Tommy Miller',
       text: 'A caring and compassionate man, Tommy survived the early years of the outbreak with Joel. However, the two separated as Tommy sought a more meaningful life than mere survival, leading him to join the Fireflies and later seek redemption through his wife, Maria, and the community of Jackson.'
-    },    {
+    }, {
       title: 'Frank ',
       text: 'Frank was a character who appeared postmortem in The Last of Us. It is heavily implied he was Bill\'s partner and lover. After becoming infected, Frank took his own life by hanging.'
     },
@@ -46,10 +48,10 @@ export class AccordionpageComponent {
     {
       title: 'David ',
       text: 'David was the main antagonist of the winter chapter in The Last of Us, appearing as the leader of a group of cannibalistic survivors. Very little is known about David\'s past. At some point before the events of the game, he came to lead a group of survivors. He befriended James and the two came to trust each other.'
-    },    {
+    }, {
       title: 'Bill',
       text: 'Bill is a supporting character in The Last of Us. He is a survivor who maintains an uneasy alliance with Joel and Ellie.Bill was well-equipped for survival before the Cordyceps brain infection outbreak, having obtained mechanical skills at some point in his life as he is able to repair vehicles and build things from scraps. He also adapted to the new world and became a competent scavenger. To protect himself from the infected and hunters, Bill constructed many barricades and traps around his hometown'
-    },    {
+    }, {
       title: 'Gerald "Jerry" Anderson',
       text: 'Gerald "Jerry" Anderson is a minor character in The Last of Us and a supporting character in The Last of Us Part II. He was the father of Abby and the lead doctor among the Fireflies who was trying to find a cure for the Cordyceps brain infection. A recording by him can be found detailing Ellie\'s immunity in St. Mary\'s Hospital. Prior to the Cordyceps brain infection outbreak, Jerry Anderson lived in the state of Utah and attended the Northern Utah Medical University, where he graduated with a Bachelor of Science in Biology degree in 2007.[5] After the outbreak, he spent many years among the Fireflies under their leader Marlene attempting to find a cure. At some point, he had a daughter, Abby Anderson, and raised her as member of the Fireflies. Within the group, he also looked after people around Abby\'s age, such as Manny Alvarez, Owen Moore and Mel, who he began to train as an understudy. Jerry was also an avid coin collector, a trait his daughter soon picked up'
     },
